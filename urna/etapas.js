a013bcb59d0a52ccadb2d2390ad97936e6ff253b
Ruan Vital <105391213+ruanvital@users.noreply.github.com>
2022-08-18 14:54:11 -0300

    Add files via upload

---
+++ urna/etapas.js
@@ -0,0 +1,70 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38171',
+                nome: 'DON CORLEONE',
+                partido: 'ABC',
+                fotos: [
+                    {url:'don-vito.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '50111',
+                nome: 'SAUL GOODMAN',
+                partido: 'BCS',
+                fotos: [
+                    {url:'saul-goodman.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '88123',
+                nome: 'GUS FRING',
+                partido: 'LPOLLOSH',
+                fotos: [
+                    {url:'gustavo-fring.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '38',
+                nome: 'PALPATINE',
+                partido: 'IMPERIO',
+                vice: 'DARTH VADER',
+                fotos: [
+                    {url:'palpatine.jpg', legenda: 'Prefeito'},
+                    {url:'darth.jpg', legenda: 'Vice-Prefeito', vice: true}
+                ]
+            },
+            {
+                numero: '72',
+                nome: 'LEX LUTHOR',
+                partido: 'PGG',
+                vice: 'NORMAN OSBORN',
+                fotos: [
+                    {url:'lex-luthor.jpg', legenda: 'Prefeito'},
+                    {url:'norman-osborn.jpg', legenda: 'Vice-Prefeito', vice: true}
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'WILSON FISK',
+                partido: 'KINGPIN',
+                vice: 'OTTO OCTAVIUS',
+                fotos: [
+                    {url:'wilson-fisk.jpg', legenda: 'Prefeito'},
+                    {url:'octavius.jpg', legenda: 'Vice-Prefeito', vice: true}
+                ]
+            }
+        ]
+
+    },
+]
